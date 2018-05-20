85569e3307107710f6dbdfd771b3c5113956a93e
YinTan <yntantan@icloud.com>
2018-05-20 13:45:24 +0800

    A stupid method of delay execution

--- base_serial.py
+++ base_serial.py
@@ -1,5 +1,8 @@
 import serial
 import struct
+import time
+import threading
+from threading import Timer
 
 DEFAULT_SERIAL_PATH = "/dev/tty.wchusbserial1420"
 DEFAULT_BAUD_RATE = 9600
@@ -11,12 +14,27 @@ class Servo:
         self.path = path
         self.baud_rate = baud_rate
         self.ser = serial.Serial(self.path, self.baud_rate, timeout=1)
+        self.last_run_time = 0
+        self.last_time = time.time()
         #header length command count time (id angle) * 6
         #  2      1       1      1     2    1   2
 
-    def move(self, time, value):
-        if time is None or value is None:
+    def wait_and_move(self, t, value):
+        if t is None or value is None:
             return
+        if self.last_time + self.last_run_time < time.time():
+            self._move(t, value)
+        else:
+            interval = self.last_time + self.last_run_time - time.time()
+            if interval <= 0:
+                self._move(t, value)
+            else:
+                Timer(interval/1000, self._move, (t, value)).start()
+        self.last_time += t
+        self.last_run_time = t
+
+    def _move(self, time, value):
+        print(threading.current_thread())
         count = len(value)
         s = struct.Struct("=HBBBH" + "BH" * count)
         header = [21845, count*3+5, 3, count, time]
@@ -32,9 +50,13 @@ class Servo:
 
 if __name__ == '__main__':
     s = Servo()
-    s.move(2000, {1:500,2:500})
-    # s.move(1000, {2:2500})
-    # s.move(1000, {2:1000})
-    # s.move(500, {1:1500})
-    # s.move(500, {1:500})
-    # s.move(500, {1:1500})
+    s.wait_and_move(2000, {2:500})
+    s.wait_and_move(2000, {2:2500})
+    s.wait_and_move(2000, {2:500})
+    s.wait_and_move(2000, {1:500})
+    s.wait_and_move(2000, {1:1500})
+    s.wait_and_move(2000, {1:500})
+    s.wait_and_move(2000, {3:500, 4:2500})
+    s.wait_and_move(2000, {3:1500, 4:1500})
+    s.wait_and_move(2000, {3:2500, 4:500})
+    print("end")
